6d2eb53a1117c7ded128d8d4fc3e4708a70a06b2
zuo <2417389455@qq.com>
2022-06-08 15:34:01 +0800

    删除语句

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -4,8 +4,6 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("测试");
-        System.out.println("MASTER");
-        System.out.println("dev");
-
+        System.out.println("测试123123123123");
     }
 }
